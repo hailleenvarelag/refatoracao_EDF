99390695f351827b245fe9b163e131d6811354a3
hailleenvarelag <seu-email-no-github@example.com>
2025-02-03 14:57:44 -0300

    Descrição das mudanças

--- alteracoes_base_implementacao.py
+++ alteracoes_base_implementacao.py
@@ -7,7 +7,7 @@ class ImplementandoAlteracoesBase(AlteracoesBase):
         
         self.df.loc[self.df[0] == '0000', 1] = '006'
         self.df.loc[self.df[0] == '0000', 2] = '1'
-        #Falta modificaar o campo 4
+        #Falta modificar o campo 4
 
 
         self.df.loc[self.df[0] == '0100', 1] = 'WILLIAM SILVA DE ALMEIDA'
@@ -21,34 +21,49 @@ class ImplementandoAlteracoesBase(AlteracoesBase):
         self.df.loc[self.df[0] == '0100', 13] = '5300108' 
 
     def calculando_contadores_de_linhas(self):
-
+    # Contar quantas vezes '9900' aparece
         contagem_99_00 = self.df.loc[self.df[0] == '9900', 0].value_counts()
-        
 
+        # Encontrar os índices das linhas '9001' e '9999'
         start_index = self.df.index[self.df[0].str.startswith('9001')].min()
         end_index = self.df.index[self.df[0].str.startswith('9999')].max()
 
-        # Fazer o slice do DataFrame usando os índices identificados
+        # Criar um subconjunto do DataFrame entre '9001' e '9999'
         subset_df = self.df.loc[start_index:end_index]
 
         # Contar o número de linhas no subset
         contagem_linhas_99_90 = len(subset_df)
 
-        contagem_total_linhas = len(self.df) -1
-        
-        self.df.loc[self.df[0] == '9999',1 ] = contagem_total_linhas
+        # Calcular total de linhas -1
+        contagem_total_linhas = len(self.df) - 1
 
-        self.df.loc[(self.df[0] == '9900') & (self.df[1] == '9900'),2] = contagem_99_00.values[0]
+        # Atualizar valores em '9999'
+        self.df.loc[self.df[0] == '9999', 1] = contagem_total_linhas
 
-        self.df.loc[(self.df[0] == '9990' ),1] = contagem_linhas_99_90
+        # Atualizar contagem dentro de '9900'
+        self.df.loc[(self.df[0] == '9900') & (self.df[1] == '9900'), 2] = contagem_99_00.values[0]
 
-        contador_M = self.df[self.df[0].str.startswith('M')].shape[0]
+        # Atualizar contagem de linhas para '9990'
+        self.df.loc[self.df[0] == '9990', 1] = contagem_linhas_99_90
+
+        # Excluir M210 e M610 da contagem de 'M'
+        contador_M = self.df[(self.df[0].str.startswith('M')) & (~self.df[0].isin(['M210', 'M610']))].shape[0]
+
+        # Contar as linhas que começam com 'F'
         contador_F = self.df[self.df[0].str.startswith('F')].shape[0]
 
-        print('---------- LOG Contador de linhas para Rubrica M => ',contador_M)
-        print('---------- LOG Contador de linhas para Rubrica M => ',contador_F)
-        
-        self.df.loc[self.df[0] == 'M990',1] = contador_M
-        self.df.loc[self.df[0] == 'F990',1] = contador_F
-       
-        self.df = pd.concat([self.df, pd.DataFrame([[''] * len(self.df.columns)], columns=self.df.columns)], ignore_index=True)         
+        # Exibir logs no console
+        print('---------- LOG Contador de linhas para Rubrica M (excluindo M210 e M610) => ', contador_M)
+        print('---------- LOG Contador de linhas para Rubrica F => ', contador_F)
+
+        # Atualizar contagem em M990 e F990
+        self.df.loc[self.df[0] == 'M990', 1] = contador_M
+        self.df.loc[self.df[0] == 'F990', 1] = contador_F
+
+        # Atualizar corretamente '9900' removendo M210 e M610
+        for rubrica in ['M210', 'M610']:
+            self.df = self.df[~((self.df[0] == '9900') & (self.df[1] == rubrica))]
+
+        # Adicionar uma linha em branco no final
+        self.df = pd.concat([self.df, pd.DataFrame([[''] * len(self.df.columns)], columns=self.df.columns)], ignore_index=True)
+         
